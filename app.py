e255d2ac0d517ada1e86aab6043097c7cd0ec17f
jules201 <julvoss@uos.de>
2024-11-15 17:19:49 +0100

    added background image

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
 import streamlit as st
+import base64
 
-st.title(":green[Welcome to the _fruit Guessing Game_ :lemon:]")
+st.title("Welcome to the _fruit Guessing Game_ :lemon:")
 
 st.write("This is my first web app.")
-import streamlit as st
 
+def get_img_as_base64(file):
+    with open(file, "rb") as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+
+img = get_img_as_base64("./fruits.jpeg")
+page_bg_img = f"""
+<style>
+[data-testid="stAppViewContainer"]{{
+background-image: url("data:image/png;base64,{img}");
+background-size: cover;
+
+}}
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
