613923b3550efb9f3a077bef8dcea3cf6424cb6b
jules201 <julvoss@uos.de>
2024-11-15 17:28:28 +0100

    comments

--- app.py
+++ app.py
@@ -5,13 +5,15 @@ st.title("Welcome to the _fruit Guessing Game_ :lemon:")
 
 st.write("This is my first web app.")
 
+#encodes picture to binary data and encodes it in base 64 text format
 def get_img_as_base64(file):
     with open(file, "rb") as f:
         data = f.read()
     return base64.b64encode(data).decode()
 
-
 img = get_img_as_base64("./fruits.jpeg")
+
+#altering background image 
 page_bg_img = f"""
 <style>
 [data-testid="stAppViewContainer"]{{
@@ -21,5 +23,4 @@ background-size: cover;
 }}
 </style>
 """
-
 st.markdown(page_bg_img, unsafe_allow_html=True)
